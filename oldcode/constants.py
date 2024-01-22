c9ac4fa0813c9a9a5934b18f3bdb391908c6c994
benopp99 <50880489+benopp99@users.noreply.github.com>
2024-01-22 14:03:07 -0700

    Create constants.py

---
+++ oldcode/constants.py
@@ -0,0 +1,12 @@
+G_Grav = 6.674e-08
+K_Boltz = 1.381e-16
+M_P = 1.673e-24
+C_S = 2.9989e+10
+M_Solar = 1.989e+33
+cmpermpc = 3.086e+24
+cmperkpc = 3.086e+21
+XH = 0.752
+omega_b = 0.04825
+omega_M = 0.307
+mu = 0.59
+OtoH_Solar = 10**-3.31
